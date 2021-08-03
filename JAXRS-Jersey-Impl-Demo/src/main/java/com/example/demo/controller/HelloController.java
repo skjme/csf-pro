1e507654ceb6c7e71b3e9c4a8cd3bf60654aa4a9
chaseshu <chase.shu@asia.training>
2021-08-03 13:16:53 +0800

    Update HelloController.java

--- JAXRS-Jersey-Impl-Demo/src/main/java/com/example/demo/controller/HelloController.java
+++ JAXRS-Jersey-Impl-Demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -2,7 +2,11 @@ package com.example.demo.controller;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriInfo;
 
 
 @Path("/hello")
@@ -11,6 +15,18 @@ public class HelloController {
     @GET
     public String hello(@QueryParam("name") String name){
         System.out.println(name);
-        return "hello" + "name";
+        return "hello，" + name;
+    }
+    
+    @GET
+    @Path("/query")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String listQueryParamNames(@Context UriInfo uriInfo){
+        StringBuilder buf = new StringBuilder();
+        for(String param: uriInfo.getQueryParameters().keySet()){
+            buf.append(param);
+            buf.append("\n");
+        }
+        return buf.toString();
     }
 }
